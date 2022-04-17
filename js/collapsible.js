b70603419527983337bd548d4b1c0dc2d0121799
ferrelanthoni <ferrelanthoni@gmail.com>
2022-04-16 22:32:16 -0700

    Change from collapsible items to card project items. Changed profile picture to profile memoji

--- js/collapsible.js
+++ js/collapsible.js
@@ -1,14 +1,7 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
+function onProfileHover(element) {
+    element.setAttribute('src', "asset/profile2.png");
+}
 
-for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.maxHeight){
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-        } 
-    });
+function onProfleLeave(element) {
+    element.setAttribute('src', "asset/profile.png");
 }
